4f4b00bd790a4806118760f4e028f8cdd1213c7d
Shiming Zhang <wzshiming@foxmail.com>
2022-08-26 13:20:40 +0800

    Fix supported_releases.sh

--- hack/supported_releases.sh
+++ hack/supported_releases.sh
@@ -24,9 +24,26 @@ record="${ROOT_DIR}/supported_releases.txt"
 # For compatibility with the Kind runtime, get the labels from Kind's node images
 function latest_releases() {
   local ref_image=kindest/node
-  token=$(curl "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ref_image}:pull" | jq -r '.token')
-  tags=$(curl "https://registry-1.docker.io/v2/${ref_image}/tags/list" -H "Authorization: Bearer ${token}" | jq -r '.tags | .[]')
-  echo "${tags}" | grep -e '^v\d\+\.\d\+\.\d\+$'
+  local auth_data
+  local token
+  local list_data
+  local tags
+  auth_data="$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ref_image}:pull")"
+  token="$(echo "${auth_data}" | jq -r '.token')"
+  if [[ "${token}" == "" ]]; then
+    echo "Failed get token" >&2
+    echo "${auth_data}" >&2
+    return 1
+  fi
+
+  list_data="$(curl -s "https://registry-1.docker.io/v2/${ref_image}/tags/list" -H "Authorization: Bearer ${token}")"
+  tags="$(echo "${list_data}" | jq -r '.tags | .[]' | grep -e '^v[0-9]\+\.[0-9]\+\.[0-9]\+$' | sed 's/v//g')"
+  if [[ "${tags}" == "" ]]; then
+    echo "Failed get list" >&2
+    echo "${list_data}" >&2
+    return 1
+  fi
+  echo "${tags}"
 }
 
 # Get historical supported releases
@@ -58,7 +75,8 @@ function send_pr() {
   local status
   local branch
   local body
-  local latest
+  local diff_data
+
   status=$(git status -s)
   if [[ "${status}" == "" ]]; then
     echo "No modification"
@@ -86,6 +104,8 @@ function send_pr() {
     git config --global user.name "bot"
   fi
 
+  diff_data="$(git diff)"
+
   git add supported_releases.txt
   git commit -m "Bump supported_releases.txt"
   if ! git push --set-upstream origin "${branch}"; then
@@ -96,7 +116,7 @@ function send_pr() {
   body="
 \`\`\` console
 
-$(git diff)
+${diff_data}
 
 \`\`\`
 "
@@ -104,11 +124,23 @@ $(git diff)
 }
 
 function main() {
-  out="$(bump $(record_releases) $(latest_releases) | sort --reverse --version-sort)"
+  local record_data
+  local latest_data
+  latest_data="$(latest_releases)"
+
+  if [[ "${latest_data}" == "" ]]; then
+    echo "Failed get latest releases"
+    return 1
+  fi
+
+  record_data="$(record_releases)"
+  out="$(bump ${record_data} ${latest_data} | sort --reverse --version-sort)"
 
   if [[ "${out}" == "$(record_releases)" ]]; then
+    echo "No update"
     return 0
   fi
+
   echo "${out}" >"${record}"
 
   if [[ "${SEND_PR}" == "true" ]]; then
