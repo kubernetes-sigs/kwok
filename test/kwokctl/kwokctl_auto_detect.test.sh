e769d0002c5e8795057fb41f3f893ae5a4c709f9
Wei Zhang <kweizh@gmail.com>
2023-02-09 01:06:47 +0800

    test: add runtime auto detect test
    Signed-off-by: Wei Zhang <kweizh@gmail.com>

---
+++ test/kwokctl/kwokctl_auto_detect.test.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+
+source "${DIR}/helper.sh"
+
+function main() {
+  local all_releases=("${@}")
+  for release in "${all_releases[@]}"; do
+    KWOK_KUBE_VERSION="${release}" kwokctl -v=-4 create cluster --timeout 10m --wait 10m --quiet-pull --config "${DIR}"/kwokctl-config-runtimes.yaml || exit 1
+    kwokctl delete cluster || exit 1
+  done
+}
+
+requirements_for_binary
+
+main $(supported_releases)
