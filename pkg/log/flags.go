d92ee8667ff4bfc3f33a8f57a3d90b9becc87904
Shiming Zhang <wzshiming@foxmail.com>
2023-03-28 15:27:33 +0800

    Support parse string for flag -v

--- pkg/log/flags.go
+++ pkg/log/flags.go
@@ -25,8 +25,29 @@ import (
 
 // InitFlags initializes the flags for the log.
 func InitFlags(ctx context.Context, flags *pflag.FlagSet) (context.Context, *Logger) {
-	v := flags.IntP("v", "v", 0, "number for the log level verbosity")
+	var level levelFlagValue
+	flags.VarP(&level, "v", "v", "number for the log level verbosity (DEBUG, INFO, WARN, ERROR) or (-4, 0, 4, 8)")
 	_ = flags.Parse(os.Args[1:])
-	logger := NewLogger(os.Stdout, Level(*v))
+	l := Level(level)
+	logger := NewLogger(os.Stdout, l)
 	return NewContext(ctx, logger), logger
+}
+
+type levelFlagValue Level
+
+func (l levelFlagValue) String() string {
+	return Level(l).String()
+}
+
+func (l *levelFlagValue) Set(v string) error {
+	n, err := ParseLevel(v)
+	if err != nil {
+		return err
+	}
+	*l = levelFlagValue(n)
+	return nil
+}
+
+func (l levelFlagValue) Type() string {
+	return "log-level"
 }
--- site/content/en/docs/generated/kwokctl.md
+++ site/content/en/docs/generated/kwokctl.md
@@ -12,7 +12,7 @@ kwokctl [command] [flags]
   -c, --config stringArray   config path (default [~/.kwok/kwok.yaml])
   -h, --help                 help for kwokctl
       --name string          cluster name (default "kwok")
-  -v, --v int                number for the log level verbosity
+  -v, --v log-level          number for the log level verbosity (DEBUG, INFO, WARN, ERROR) or (-4, 0, 4, 8) (default INFO)
 ```
 
 ### SEE ALSO
