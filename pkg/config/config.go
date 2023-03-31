30ab0625d9db001f563f807dfcd3662302137251
Nikola Jokic <jokicnikola07@gmail.com>
2023-03-31 11:47:29 +0200

    reorder Load in config to be more understandable after merge conflict resolution

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -166,17 +166,6 @@ func Load(ctx context.Context, src ...string) ([]metav1.Object, error) {
 				return nil, err
 			}
 			objs = append(objs, out)
-		case v1alpha1.ClusterLogsKind:
-			obj := &v1alpha1.ClusterLogs{}
-			err = json.Unmarshal(raw, &obj)
-			if err != nil {
-				return nil, err
-			}
-			out, err := internalversion.ConvertToInternalClusterLogs(obj)
-			if err != nil {
-				return nil, err
-			}
-			objs = append(objs, out)
 		case v1alpha1.ExecKind:
 			obj := &v1alpha1.Exec{}
 			err = json.Unmarshal(raw, &obj)
@@ -184,6 +173,17 @@ func Load(ctx context.Context, src ...string) ([]metav1.Object, error) {
 				return nil, err
 			}
 			out, err := internalversion.ConvertToInternalExec(obj)
+			if err != nil {
+				return nil, err
+			}
+			objs = append(objs, out)
+		case v1alpha1.ClusterLogsKind:
+			obj := &v1alpha1.ClusterLogs{}
+			err = json.Unmarshal(raw, &obj)
+			if err != nil {
+				return nil, err
+			}
+			out, err := internalversion.ConvertToInternalClusterLogs(obj)
 			if err != nil {
 				return nil, err
 			}
@@ -213,12 +213,12 @@ func Load(ctx context.Context, src ...string) ([]metav1.Object, error) {
 
 // Save saves the given objects to the given path.
 func Save(ctx context.Context, path string, objs []metav1.Object) error {
-	err := os.MkdirAll(filepath.Dir(path), 0750)
+	err := os.MkdirAll(filepath.Dir(path), 0o750)
 	if err != nil {
 		return err
 	}
 
-	file, err := os.OpenFile(filepath.Clean(path), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0640)
+	file, err := os.OpenFile(filepath.Clean(path), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o640)
 	if err != nil {
 		return err
 	}
