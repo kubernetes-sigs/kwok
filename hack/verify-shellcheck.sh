201ae14430fad1aa99cb6ef6650a5dd9eab87cc8
Zhuzhenghao <zhenghao.zhu@daocloud.io>
2023-04-11 16:04:12 +0800

    Add shellcheck to check shell scripts

---
+++ hack/verify-shellcheck.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+# Copyright 2023 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+ROOT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")"/..)"
+
+# disabled lints
+disabled=(
+  # this lint disallows non-constant source, which we use extensively without
+  # any known bugs
+  1090
+  # this lint warns when shellcheck cannot find a sourced file
+  # this wouldn't be a bad idea to warn on, but it fails on lots of path
+  # dependent sourcing, so just disable enforcing it
+  1091
+  # this lint prefers command -v to which, they are not the same
+  2230
+  # TODO:
+  2002
+  2009
+  2015
+  2034
+  2038
+  2046
+  2053
+  2069
+  2076
+  2086
+  2126
+  2181
+  2207
+  2294
+)
+# comma separate for passing to shellcheck
+join_by() {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
+SHELLCHECK_DISABLED="$(join_by , "${disabled[@]}")"
+readonly SHELLCHECK_DISABLED
+
+all_shell_scripts=()
+while IFS=$'\n' read -r script; do
+  git check-ignore -q "$script" || all_shell_scripts+=("$script")
+done < <(find . -name "*.sh" \
+  -not \( \
+  -path ./.git\* -o \
+  -path ./vendor\* \
+  \))
+
+SHELLCHECK_OPTIONS=(
+  "--external-sources"
+  "--exclude=${SHELLCHECK_DISABLED}"
+  "--color=always"
+)
+
+COMMAND=()
+if command -v shellcheck; then
+  COMMAND=(shellcheck)
+elif command -v docker; then
+  COMMAND=(
+    docker run
+    --rm
+    -v "${ROOT_DIR}:${ROOT_DIR}"
+    -w "${ROOT_DIR}"
+    docker.io/koalaman/shellcheck-alpine:v0.8.0@sha256:f42fde76d2d14a645a848826e54a4d650150e151d9c81057c898da89a82c8a56
+    shellcheck
+  )
+else
+  echo "WARNING: shellcheck or docker not installed" >&2
+  exit 1
+fi
+
+cd "${ROOT_DIR}" && "${COMMAND[@]}" "${SHELLCHECK_OPTIONS[@]}" "${all_shell_scripts[@]}"
