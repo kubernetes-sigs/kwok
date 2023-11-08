c7a211d2b477e73b28b5c59f5951e7f1dcd69453
Shiming Zhang <wzshiming@foxmail.com>
2023-11-08 11:03:10 +0800

    Move authorization to dryrun

--- test/kwokctl/kwokctl_with_dryrun.test.sh
+++ test/kwokctl/kwokctl_with_dryrun.test.sh
@@ -30,7 +30,9 @@ function main() {
     name="dryrun-cluster-${runtime}"
     echo "------------------------------"
     echo "Testing dryrun on runtime ${runtime}"
-    got="$(KWOK_RUNTIME="${runtime}" create_cluster "${name}" "" --dry-run | clear_testdata "${name}")"
+    got="$(KWOK_RUNTIME="${runtime}" create_cluster "${name}" "" \
+      --kube-authorization=false \
+      --dry-run | clear_testdata "${name}")"
     want="$(<"${DIR}/testdata/${runtime}/create_cluster.txt")"
     if [[ "${got}" != "${want}" ]]; then
       echo "------------------------------"
