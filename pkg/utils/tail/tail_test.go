a54053c0162c9d16ce9aa14eaeb17e04bbbde7c4
Joeyyy09 <imt_2021063@iiitm.ac.in>
2024-05-19 11:11:22 +0530

    Update Test Cases for tail package
    Signed-off-by: Joeyyy09 <imt_2021063@iiitm.ac.in>
    
    Made faulty reader a field to be more generic
    
    Signed-off-by: Joeyyy09 <imt_2021063@iiitm.ac.in>

--- pkg/utils/tail/tail_test.go
+++ pkg/utils/tail/tail_test.go
@@ -36,6 +36,7 @@ func TestFindTailLineStartIndex(t *testing.T) {
 	strLastLineWithEol := strLastLineNoEol + "\n"
 	fNoEol := strings.NewReader(strLastLineNoEol)
 	fWithEol := strings.NewReader(strLastLineWithEol)
+
 	type args struct {
 		f io.ReadSeeker
 		n int64
@@ -91,7 +92,6 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			want:    0,
 			wantErr: false,
 		},
-
 		{
 			name: "last line with eol, last line",
 			args: args{
@@ -102,7 +102,7 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "last line without eol, all",
+			name: "last line with eol, all",
 			args: args{
 				f: fWithEol,
 				n: 10,
@@ -111,7 +111,7 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "last line without eol, get first line",
+			name: "last line with eol, get first line",
 			args: args{
 				f: fWithEol,
 				n: 9,
@@ -120,7 +120,7 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name: "last line without eol, over 10 lines",
+			name: "last line with eol, over 10 lines",
 			args: args{
 				f: fWithEol,
 				n: 11,
@@ -137,6 +137,52 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			want:    0,
 			wantErr: false,
 		},
+		// Edge cases
+		{
+			name: "empty file",
+			args: args{
+				f: strings.NewReader(""),
+				n: 1,
+			},
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name: "single line file without eol",
+			args: args{
+				f: strings.NewReader("single line"),
+				n: 1,
+			},
+			want:    0,
+			wantErr: false,
+		},
+		{
+			name: "file with multiple lines within one block",
+			args: args{
+				f: strings.NewReader("line1\nline2\nline3\n"),
+				n: 2,
+			},
+			want:    6,
+			wantErr: false,
+		},
+		{
+			name: "error on seek",
+			args: args{
+				f: &faultyReader{data: []byte("line1\nline2\nline3\n"), seekErr: true},
+				n: 1,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			name: "error on read",
+			args: args{
+				f: &faultyReader{data: []byte("line1\nline2\nline3\n"), readErr: true},
+				n: 1,
+			},
+			want:    0,
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -150,4 +196,37 @@ func TestFindTailLineStartIndex(t *testing.T) {
 			}
 		})
 	}
+}
+
+// faultyReader simulates errors for testing error handling in FindTailLineStartIndex.
+type faultyReader struct {
+	data    []byte
+	seekErr bool
+	readErr bool
+	pos     int64
+}
+
+func (f *faultyReader) Seek(offset int64, whence int) (int64, error) {
+	if f.seekErr {
+		return 0, fmt.Errorf("seek error")
+	}
+	switch whence {
+	case io.SeekStart:
+		f.pos = offset
+	case io.SeekCurrent:
+		f.pos += offset
+	case io.SeekEnd:
+		f.pos = int64(len(f.data)) + offset
+	}
+	return f.pos, nil
+}
+
+func (f *faultyReader) Read(p []byte) (n int, err error) {
+	if f.readErr {
+		return 0, fmt.Errorf("read error")
+	}
+	copy(p, f.data[f.pos:])
+	n = len(p)
+	f.pos += int64(n)
+	return n, nil
 }
