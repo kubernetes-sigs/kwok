ccb3f47f5b3f763e6fbd487462d98b0b136bf22a
mohamedasifs123 <142201466+mohamedasifs123@users.noreply.github.com>
2024-05-21 18:25:13 +0530

    Update lp_test.go

--- pkg/utils/exec/lp_test.go
+++ pkg/utils/exec/lp_test.go
@@ -1,7 +1,22 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package exec
 
 import (
-	"os"
 	"testing"
 )
 
@@ -19,16 +34,7 @@ func TestLookPath(t *testing.T) {
 		{"Directory", "/usr/bin", true},                       // A directory
 		{"SpecialCharacters", "weird$cmd&name", true},         // Command na              // A relative path to an existing executable
 		{"PathToNonExecutableDirectory", "/usr/lib", true},    // Full path to a non-executable directory
-		// Full path to an executable with space
-		{"ExecutableInCurrentDirectory", "./test_executable", false}, // Executable in the current directory
 	}
-
-	// Create a test executable file in the current directory
-	testFile := "test_executable"
-	if err := os.WriteFile(testFile, []byte("#!/bin/sh\necho 'Hello, World!'"), 0755); err != nil {
-		t.Fatalf("failed to create test executable file: %v", err)
-	}
-	defer os.Remove(testFile) // Clean up
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
