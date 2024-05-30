eba3615a7f55eb52ceefd77bb6787459744a972d
mohamedasifs123 <142201466+mohamedasifs123@users.noreply.github.com>
2024-05-30 22:44:01 +0530

    Add files via upload

---
+++ test/e2e/kwok/kwok-with-cni/main_test.go
@@ -0,0 +1,65 @@
+/*
+	Copyright 2023 The Kubernetes Authors.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+// Package default_test is a test environment for kwok.
+package kwok_with_cni
+
+import (
+	"os"
+	"testing"
+
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
+	"sigs.k8s.io/e2e-framework/support/kind"
+
+	"sigs.k8s.io/kwok/pkg/consts"
+	"sigs.k8s.io/kwok/pkg/utils/path"
+	"sigs.k8s.io/kwok/test/e2e/helper"
+)
+
+var (
+	testEnv     env.Environment
+	pwd         = os.Getenv("PWD")
+	rootDir     = path.Join(pwd, "../../../..")
+	clusterName = envconf.RandomName("kwok-e2e", 16)
+	namespace   = envconf.RandomName("ns", 16)
+	testImage   = "localhost/kwok:test"
+	baseImage   = "registry.k8s.io/build-image/distroless-iptables:v0.5.1"
+)
+
+func TestMain(m *testing.M) {
+	testEnv = helper.Environment()
+
+	deploy := pwd
+	crs := path.Join(rootDir, "kustomize/stage/fast")
+	testEnv.Setup(
+		helper.BuildKwokBaseImage(rootDir, testImage, baseImage, consts.RuntimeTypeDocker),
+		helper.BuildKindImage(clusterName, rootDir),
+		envfuncs.CreateCluster(kind.NewProvider(), clusterName),
+		helper.WaitForAllNodesReady(),
+		envfuncs.LoadImageToCluster(clusterName, testImage),
+		helper.CreateByKustomize(deploy),
+		helper.CreateByKustomize(crs),
+		helper.WaitForAllNodesReady(),
+		helper.WaitForAllPodsReady(),
+		helper.CreateNamespace(namespace),
+	)
+	testEnv.Finish(
+		envfuncs.DestroyCluster(clusterName),
+	)
+	os.Exit(testEnv.Run(m))
+}
