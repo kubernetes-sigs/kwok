32a9464b25d5a756c7457018d96d755568d926e0
Manoramsharma <84619980+Manoramsharma@users.noreply.github.com>
2024-06-05 11:27:22 +0530

    Added unit tests for Gauge and counter metrics (#1114)
    * Added unit tests for Gauge and counter metrics
    
    Signed-off-by: Manoram Sharma <img_2020037@iiitm.ac.in>
    
    * Added tests for scheduler list and file under runtime
    
    Signed-off-by: Manoram Sharma <img_2020037@iiitm.ac.in>
    
    * Revert "Added tests for scheduler list and file under runtime"
    
    This reverts commit 5ee6b6431750fa5bbb27f85273a13d9c95dcca09.
    
    * Resolved suggested changes
    
    Signed-off-by: Manoram Sharma <img_2020037@iiitm.ac.in>
    
    * update lint
    
    Signed-off-by: Manoram Sharma <img_2020037@iiitm.ac.in>
    
    ---------
    
    Signed-off-by: Manoram Sharma <img_2020037@iiitm.ac.in>

---
+++ pkg/kwok/metrics/counter_test.go
@@ -0,0 +1,65 @@
+/*
+Copyright 2024 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestNewCounter(t *testing.T) {
+	// Define counter options
+	opts := CounterOpts{
+		Name: "test_counter",
+		Help: "This is a test counter",
+	}
+
+	c := NewCounter(opts)
+
+	// Ensure the counter starts at 0
+	if err := testutil.CollectAndCompare(c, strings.NewReader(`
+		# HELP test_counter This is a test counter
+		# TYPE test_counter counter
+		test_counter 0
+	`)); err != nil {
+		t.Fatalf("unexpected metrics for starting value: %s", err)
+	}
+
+	// Set the counter to a specific value
+	newValue := float64(42)
+	c.Set(newValue)
+	if err := testutil.CollectAndCompare(c, strings.NewReader(`
+		# HELP test_counter This is a test counter
+		# TYPE test_counter counter
+		test_counter 42
+	`)); err != nil {
+		t.Fatalf("unexpected metrics after set: %s", err)
+	}
+
+	// Set the counter to another value
+	newValue = float64(84)
+	c.Set(newValue)
+	if err := testutil.CollectAndCompare(c, strings.NewReader(`
+		# HELP test_counter This is a test counter
+		# TYPE test_counter counter
+		test_counter 84
+	`)); err != nil {
+		t.Fatalf("unexpected metrics after second set: %s", err)
+	}
+}
