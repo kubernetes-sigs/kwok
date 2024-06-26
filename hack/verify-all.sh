b011f5337f2a135cb2e7db6f8859de4fe76f1d29
Neeraj Nagure <nagureneeraj@gmail.com>
2024-06-26 14:45:13 +0530

    fixed conflict

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -98,6 +98,10 @@ if [[ "${VERIFY_STAGES:-true}" == "true" ]]; then
   "${ROOT_DIR}"/hack/verify-stages.sh || failed+=(stages)
 fi
 
+
+
+
+
 if [[ "${VERIFY_DRY_RUN_TESTDATA:-true}" == "true" ]]; then
   echo "[*] Verifying testdata..."
   "${ROOT_DIR}"/hack/verify-testdata.sh || failed+=(testdata)
