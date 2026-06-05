7336597cb9d77ee876c4a6980cf69baf2e970a13
Shiming Zhang <wzshiming@hotmail.com>
2026-06-05 15:49:36 +0800

    flagset.ParseErrorsWhitelist is deprecated: use FlagSet.ParseErrorsAllowlist instead

--- cmd/kwokctl/main.go
+++ cmd/kwokctl/main.go
@@ -34,7 +34,7 @@ import (
 
 func main() {
 	flagset := pflag.NewFlagSet("global", pflag.ContinueOnError)
-	flagset.ParseErrorsWhitelist.UnknownFlags = true
+	flagset.ParseErrorsAllowlist.UnknownFlags = true
 	flagset.Usage = func() {}
 
 	ctx := signals.SetupSignalContext()
