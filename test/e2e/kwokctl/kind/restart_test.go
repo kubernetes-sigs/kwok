ea1296e8f1ad4aa957dfd1066740146288208c67
Shiming Zhang <wzshiming@hotmail.com>
2026-06-25 12:10:37 +0800

    metrics-server reorder kubelet-preferred-address-types to prioritize Hostname

---
+++ test/e2e/kwokctl/kind/restart_test.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kind_test
+
+import (
+	"testing"
+
+	"sigs.k8s.io/kwok/test/e2e"
+)
+
+func TestRestart(t *testing.T) {
+	t.Skip("Skip test due to Kind failing to start Load Kernel Modules: https://github.com/kubernetes-sigs/kind/issues/3283")
+
+	f0 := e2e.CaseRestart(kwokctlPath, clusterName, runtimeEnv, rootDir, updateTestdata).
+		Feature()
+	testEnv.Test(t, f0)
+}
+
+func TestSnapshot(t *testing.T) {
+	f0 := e2e.CaseSnapshot(kwokctlPath, clusterName, runtimeEnv, rootDir, updateTestdata, pwd).
+		Feature()
+	testEnv.Test(t, f0)
+}
