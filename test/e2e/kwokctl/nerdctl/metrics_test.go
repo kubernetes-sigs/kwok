5d5f3511526e3450f3e861871b4fc6ef7c0e9e6f
Shiming Zhang <wzshiming@hotmail.com>
2026-07-02 14:07:57 +0800

    Add e2e for metrics

---
+++ test/e2e/kwokctl/nerdctl/metrics_test.go
@@ -0,0 +1,37 @@
+/*
+Copyright 2026 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nerdctl_test
+
+import (
+	"testing"
+
+	"sigs.k8s.io/kwok/pkg/consts"
+	"sigs.k8s.io/kwok/test/e2e"
+)
+
+func TestMetrics(t *testing.T) {
+	f0 := e2e.CaseMetrics(
+		consts.ComponentEtcd,
+		consts.ComponentKubeApiserver,
+		consts.ComponentKubeControllerManager,
+		consts.ComponentKubeScheduler,
+		consts.ComponentKwokController,
+		consts.ComponentPrometheus,
+		consts.ComponentMetricsServer,
+	).Feature()
+	testEnv.Test(t, f0)
+}
